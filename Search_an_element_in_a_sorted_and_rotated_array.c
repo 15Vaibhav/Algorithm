6dab898aa0f932121cd77309de3739c37450a180
Vaibhav <vp612512@gmail.com>
2019-06-22 15:22:54 +0530

    Update Search_an_element_in_a_sorted_and_rotated_array.c

--- Search_an_element_in_a_sorted_and_rotated_array.c
+++ Search_an_element_in_a_sorted_and_rotated_array.c
@@ -1,33 +1,49 @@
-#include <stdio.h>
- 
-void search(int a[],int s,int l,int n){
-	int x;
-	x = (s+l)/2;
-	if(a[x]==n){
-		printf("number is %d and index is %d ",n,x);
-	}
-	else if(a[x]>a[s]){
-		if(n<a[s]||n>a[x]){
-		search(a,x+1,l,n);	
-		}
-		else{
-			search(a,s,x-1,n);
-		}
-	}
-	else if(a[x]<a[l]){
-		if(n>a[l]||n<a[x]){
-			search(a,s,x-1,n);
-		}else{
-			search(a,x+1,l,n);
-		}
-	}
-	else{
-		printf("%d not found",n);
-	}
-	}
-int main(void) {
-	int a[] = {30, 40, 50, 10, 20},l;
-	l = sizeof(a)/sizeof(a[0]);
-	search(a,0,l-1,10);
-	return 0;
-}
+// Search an element in sorted and rotated 
+// array using single pass of Binary Search 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Returns index of key in arr[l..h] if 
+// key is present, otherwise returns -1 
+int search(int arr[], int l, int h, int key) 
+{ 
+	if (l > h) return -1; 
+
+	int mid = (l+h)/2; 
+	if (arr[mid] == key) return mid; 
+
+	/* If arr[l...mid] is sorted */
+	if (arr[l] <= arr[mid]) 
+	{ 
+		/* As this subarray is sorted, we can quickly 
+		check if key lies in half or other half */
+		if (key >= arr[l] && key <= arr[mid]) 
+		return search(arr, l, mid-1, key); 
+		/*If key not lies in first half subarray, 
+		Divide other half into two subarrays, 
+		such that we can quickly check if key lies 
+		in other half */
+		return search(arr, mid+1, h, key); 
+	} 
+
+	/* If arr[l..mid] first subarray is not sorted, then arr[mid... h] 
+	must be sorted subarray */
+	if (key >= arr[mid] && key <= arr[h]) 
+		return search(arr, mid+1, h, key); 
+
+	return search(arr, l, mid-1, key); 
+} 
+
+// Driver program 
+int main() 
+{ 
+	int arr[] = {4, 5, 6, 7, 8, 9, 1, 2, 3}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	int key = 6; 
+	int i = search(arr, 0, n-1, key); 
+
+	if (i != -1) 
+	cout << "Index: " << i << endl; 
+	else
+	cout << "Key not found"; 
+} 
